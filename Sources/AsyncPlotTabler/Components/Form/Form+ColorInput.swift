a79347edfe6d7b4066cef732ab797cea1a69b6fe
Brandon <32753167+btoms20@users.noreply.github.com>
2024-02-01 11:58:48 -0800

    Embeded in Plot.Form extension.

--- Sources/AsyncPlotTabler/Components/Form/Form+ColorInput.swift
+++ Sources/AsyncPlotTabler/Components/Form/Form+ColorInput.swift
@@ -6,44 +6,46 @@
 import Plot
 import Foundation
 
-public struct ColorInput: Component {
-    
-    public enum Style {
-        case rectangular
-        case rounded
+extension Plot.Form {
+    public struct ColorInput: Component {
         
-        var css:String {
-            switch self {
-            case .rounded: "rounded-circle"
-            case .rectangular: ""
+        public enum Style {
+            case rectangular
+            case rounded
+            
+            var css:String {
+                switch self {
+                case .rounded: "rounded-circle"
+                case .rectangular: ""
+                }
             }
         }
-    }
-    
-    let style:Style
-    let name:String
-    let colors:[Colors]
-    let defaultValue:Colors?
-    let isRequired:Bool
-    
-    public init(style: Style = .rectangular, name: String, colors: [Colors]? = nil, defaultValue: Colors? = nil, isRequired: Bool = false) {
-        self.style = style
-        self.name = name
-        self.colors = colors ?? Colors.allColors
-        self.defaultValue = defaultValue
-        self.isRequired = isRequired
-    }
-    
-    public func body() async -> Component {
-        await DivC("row g-2") {
-            for color in colors {
-                await DivC("col-auto") {
-                    await Element(name: "label") {
-                        Input(type: .radio, name: name, value: color.css, isRequired: isRequired)
-                            .class("form-colorinput-input")
-                            .optionalAttribute(color == defaultValue, name: "checked", ignoreValueIfEmpty: false)
-                        Span { }.class("form-colorinput-color \(color.backgroundCSS)").class(style.css)
-                    }.class("form-colorinput")
+        
+        let style:Style
+        let name:String
+        let colors:[Colors]
+        let defaultValue:Colors?
+        let isRequired:Bool
+        
+        public init(style: Style = .rectangular, name: String, colors: [Colors]? = nil, defaultValue: Colors? = nil, isRequired: Bool = false) {
+            self.style = style
+            self.name = name
+            self.colors = colors ?? Colors.allColors
+            self.defaultValue = defaultValue
+            self.isRequired = isRequired
+        }
+        
+        public func body() async -> Component {
+            await DivC("row g-2") {
+                for color in colors {
+                    await DivC("col-auto") {
+                        await Element(name: "label") {
+                            Input(type: .radio, name: name, value: color.css, isRequired: isRequired)
+                                .class("form-colorinput-input")
+                                .optionalAttribute(color == defaultValue, name: "checked", ignoreValueIfEmpty: false)
+                            Span { }.class("form-colorinput-color \(color.backgroundCSS)").class(style.css)
+                        }.class("form-colorinput")
+                    }
                 }
             }
         }
