edf9c6b8247f32f23f78da6064a5165bb761ad0b
Brandon <32753167+btoms20@users.noreply.github.com>
2024-02-27 09:06:49 -0800

    Added a Date.Formatted shim for Linux platforms

---
+++ Sources/AsyncPlotTabler/Utils/Date+Linux.swift
@@ -0,0 +1,101 @@
+#if os(Linux)
+
+import Foundation
+
+extension Date {
+    
+    enum DateFormatStyle {
+        case abbreviated
+        case complete
+        case long
+        case numeric
+        case omitted
+        
+        var dfString:String? {
+            switch self {
+            case .abbreviated: //Jan 17, 2021
+                "MMM d, yyyy"
+            case .complete: //Sunday, January 17, 2021
+                "EEEE, MMM d, yyyy"
+            case .long: //January 17, 2021
+                "MMMM d, yyyy"
+            case .numeric: //1/17/2021
+                "M/d/yyyy"
+            case .omitted:
+                nil
+            }
+        }
+        
+        var dateStyle:DateFormatter.Style {
+            switch self {
+            case .abbreviated: //Jan 17, 2021
+                return .medium
+            case .complete: //Sunday, January 17, 2021
+                return .full
+            case .long: //January 17, 2021
+                return .long
+            case .numeric: //1/17/2021
+                return .short
+            case .omitted:
+                return .none
+            }
+        }
+    }
+    
+    enum TimeFormatStyle {
+        case complete
+        case shortened
+        case standard
+        case omitted
+        
+        var dfString:String? {
+            switch self {
+            case .complete: // 4:03:12 PM CST
+                "h:mm:ss a Z"
+            case .shortened: // 4:03 PM
+                "h:mm a"
+            case .standard: // 4:03:12 PM
+                "h:mm:ss a"
+            case .omitted:
+                nil
+            }
+        }
+        
+        var timeStyle:DateFormatter.Style {
+            switch self {
+            case .complete: // 4:03:12 PM CST
+                return .full
+            case .shortened: // 4:03 PM
+                return .short
+            case .standard: // 4:03:12 PM
+                return .medium
+            case .omitted:
+                return .none
+            }
+        }
+    }
+    
+    func formatted(date: DateFormatStyle, time: TimeFormatStyle) -> String {
+        let df = DateFormatter()
+        df.dateStyle = date.dateStyle
+        df.timeStyle = time.timeStyle
+        df.locale = Locale.autoupdatingCurrent
+        return df.string(from: self)
+    }
+    
+    func formatted() -> String {
+        return self.formatted(date: .abbreviated, time: .shortened)
+    }
+}
+
+extension Double {
+    func formatted() -> String {
+        let nf = NumberFormatter()
+        nf.maximumFractionDigits = 5
+        nf.locale = .current
+        nf.numberStyle = .decimal
+        return nf.string(from: NSNumber(floatLiteral: self)) ?? "???"
+    }
+}
+
+#endif
